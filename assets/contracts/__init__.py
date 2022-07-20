4f07b5cb63eb36d4215739d8edaaab068f067c9f
Orcun Oruc <orcun.oruc@tu-dresden.de>
2022-07-20 10:59:19 +0200

    Organization of the repository

---
+++ assets/contracts/__init__.py
@@ -0,0 +1,5 @@
+# from .bank import bank 
+# from .deposit import deposit
+# from .reference import reference 
+# from .transfer import transfer 
+# from .withdraw import withdraw
--- venv/Lib/site-packages/pyteal/compiler/optimizer/__init__.py
+++
@@ -1,4 +0,0 @@
-from pyteal.compiler.optimizer.optimizer import (
-    OptimizeOptions,
-    apply_global_optimizations,
-)
